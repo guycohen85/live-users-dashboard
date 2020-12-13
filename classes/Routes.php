fce37ac787d198e2e9eee13c5500153bfd8b532e
guy <guycohen85@gmail.com>
2020-12-13 10:46:08 +0200

    The routes of the application

---
+++ classes/Routes.php
@@ -0,0 +1,35 @@
+<?php 
+require_once $rootFolder."/classes/Controllers/UserController.php";
+require_once $rootFolder."/classes/Controllers/AuthController.php";
+
+/** GET Request **/
+if ( $_SERVER['REQUEST_METHOD'] === 'GET' ) {
+
+    if( array_key_exists("users", $_GET) ){
+        (new UserController())->index();
+    }
+
+/** POST Request **/
+}else{
+
+    if( array_key_exists("form", $_POST) ){ //if login or register pages
+
+        if( $_POST["form"] === "register" ){
+
+            (new AuthController())->register();
+
+        }else if($_POST["form"] === "login"){
+
+            (new AuthController())->login();
+
+        }else if($_POST["form"] === "logout"){
+
+            (new AuthController())->logout();
+
+        }
+
+    }else if( array_key_exists("update_users", $_POST) ){
+        (new UserController())->update_users_status();
+    }
+
+}
